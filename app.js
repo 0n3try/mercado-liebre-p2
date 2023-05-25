958a47986278703a2811a9a7633100e89214cd72
0n3try <alva1432@gmail.com>
2023-05-24 21:09:15 -0300

    cambiando cosas del package

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.static('public'))
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Servidor corriendo en el puerto ${port}`);
 })
